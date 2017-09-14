d468207b0264e0e484e6ca34178ebfde20172c85
Jimmy Mercado <jimmy.mercado@live.com>
2017-09-14 14:12:27 -0700

    scroll to bottom if user is at the bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,24 @@
 var socket = io();
 
+function scrollToBottom() {
+  //selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
+
 socket.on('connect', function() {
   console.log('connected to server');
-
-
 });
 
 socket.on('newMessage', function(message) {
@@ -16,7 +31,7 @@ socket.on('newMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
-
+  scrollToBottom();
   // console.log('New Message:', message);
   // var formattedTime = moment(message.createdAt).format('h:mm a');
   // var li = jQuery('<li></li>');
@@ -50,6 +65,7 @@ socket.on('newLocationMessage', function(message){
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 
@@ -87,6 +103,5 @@ locationButton.on('click', function(){
     locationButton.removeAttr('disabled').text('Send Location');
     alert('Unable to fetch location.')
   });
-
 
 });
