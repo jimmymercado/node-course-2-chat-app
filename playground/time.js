b8e1745c65ac384c364cde58f1989c4b8935e6cc
Jimmy Mercado <jimmy.mercado@live.com>
2017-09-14 13:24:56 -0700

    Format timestamps using moment.js

---
+++ playground/time.js
@@ -0,0 +1,10 @@
+const moment = require('moment');
+
+
+// Jan 1, 1970 00:00:00
+
+var timeStamp = moment().valueOf();
+console.log(moment());
+
+// console.log(date.format('MMM. Do, YYYY, dddd'));
+// console.log(date.format('hh:mm:ss a'));
