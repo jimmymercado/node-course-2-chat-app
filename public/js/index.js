89472ae561a3b2bc98c790ae40d39a8c537485d7
Jimmy Mercado <jimmy.mercado@live.com>
2017-09-14 11:45:02 -0700

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -33,11 +33,13 @@ socket.on('newLocationMessage', function(message){
 jQuery('#message-form').on('submit', function(e){
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function (){
-
+    messageTextbox.val('');
   });
 });
 
@@ -48,12 +50,17 @@ locationButton.on('click', function(){
     return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr('disabled').text('Send Location');
+
     socket.emit('createLocationMessage', {
       lat: position.coords.latitude,
       lon: position.coords.longitude
     });
   }, function() {
+    locationButton.removeAttr('disabled').text('Send Location');
     alert('Unable to fetch location.')
   });
 
