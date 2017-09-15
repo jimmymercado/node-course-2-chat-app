2b5720cc7920ad9ef7b09b23317455e13dd414b2
Jimmy Mercado <jimmy.mercado@live.com>
2017-09-15 15:56:10 -0700

    Add join page that submits to chat.html

---
+++ public/js/chat.js
@@ -0,0 +1,107 @@
+var socket = io();
+
+function scrollToBottom() {
+  //selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
+
+socket.on('connect', function() {
+  console.log('connected to server');
+});
+
+socket.on('newMessage', function(message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  scrollToBottom();
+  // console.log('New Message:', message);
+  // var formattedTime = moment(message.createdAt).format('h:mm a');
+  // var li = jQuery('<li></li>');
+  // li.text(`${message.from} (${formattedTime}): ${message.text}`);
+  //
+  // jQuery('#messages').append(li);
+});
+
+socket.on('disconnect', function() {
+  console.log('disconnected from the server');
+});
+
+
+socket.on('newLocationMessage', function(message){
+  // var formattedTime = moment(message.createdAt).format('h:mm a');
+  // var li = jQuery('<li></li>');
+  // var a = jQuery('<a target="_blank">My Location</a>');
+  //
+  // li.text(`${message.from} (${formattedTime}): `);
+  // a.attr('href', message.url);
+  //
+  // li.append(a);
+  // jQuery('#messages').append(li);
+
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  scrollToBottom();
+});
+
+
+
+jQuery('#message-form').on('submit', function(e){
+  e.preventDefault();
+
+  var messageTextbox = jQuery('[name=message]');
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: messageTextbox.val()
+  }, function (){
+    messageTextbox.val('');
+  });
+});
+
+var locationButton = jQuery('#send-location');
+
+locationButton.on('click', function(){
+  if(!navigator.geolocation){
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+  navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr('disabled').text('Send Location');
+
+    socket.emit('createLocationMessage', {
+      lat: position.coords.latitude,
+      lon: position.coords.longitude
+    });
+  }, function() {
+    locationButton.removeAttr('disabled').text('Send Location');
+    alert('Unable to fetch location.')
+  });
+
+});
